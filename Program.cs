345162318ad5e4055ddaca491009faac22d5bde0
VeronikaG <gavrilova.veronika38d@gmail.com>
2026-09-13 15:45:47 +0300

    Lab02: var keyword

--- Program.cs
+++ Program.cs
@@ -39,3 +39,13 @@ decimal priceDecimal = 0.1m + 0.2m;
 
 Console.WriteLine($"double: 0.1 + 0.2 = {priceDouble}");
 Console.WriteLine($"decimal: 0.1 + 0.2 = {priceDecimal}");
+
+
+Console.WriteLine();
+Console.WriteLine("var");
+
+var studentAge = 20; // компилятор вывел int
+var gpa = 4.75; // компилятор вывел double
+var fullName = "Смирнова А.C."; // компилятор вывел string
+
+Console.WriteLine($"{fullName}, возраст {studentAge}, средний балл {gpa}");
